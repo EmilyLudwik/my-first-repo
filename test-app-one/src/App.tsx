4e0b4fad39a59b8e3c849e6bbe17fedfd40cff2f
EmilyLudwik <emilya96@hotmail.com>
2025-11-11 21:57:09 -0500

    Initial Commit

---
+++ test-app-one/src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+type Person = {
+  firstName: string;
+  lastName: string;
+  age: number;
+};
+
+const person: Person = {
+  firstName: "Emily",
+  lastName: "Ludwik",
+  age: 28
+};
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>I am {person.firstName} {person.lastName} and I am {person.age} years old</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
